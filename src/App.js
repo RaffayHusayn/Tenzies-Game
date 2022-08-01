6d2cd9926bba17cf904bdee6e052edea298bf4c3
Raffay Syed <raffayhusayn@gmail.com>
2022-08-01 04:57:13 -0400

    Add styles and components to the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Dice from "./components/Dice";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div className="dice-grid">
+        <Dice value="2" />
+        <Dice value="1" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+        <Dice value="2" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
