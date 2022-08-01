bcce894767b3b99ae33ea6a2ded86f3d1042cab9
Raffay Syed <raffayhusayn@gmail.com>
2022-08-01 05:47:42 -0400

    Add styles to Dice component

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,6 +1,10 @@
 import "../styles/Dice.css";
 function Dice(props) {
-  return <div className="dice">{props.value}</div>;
+  return (
+    <div className="dice">
+      <h2>{props.value}</h2>
+    </div>
+  );
 }
 
 export default Dice;
