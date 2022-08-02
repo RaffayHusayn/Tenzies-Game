e273b57ba73dd875a62cb26b0619972c0cc74083
Raffay Syed <raffayhusayn@gmail.com>
2022-08-02 04:36:48 -0400

    Refactor App component to take the logic out that was previously there for Dice Container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import "./App.css";
-import Dice from "./components/Dice";
-import { useState } from "react";
+import DiceContainer from "./components/DiceContainer";
 
 function App() {
-  const [dices, setDices] = useState(allDiceNumber());
-  function randomNumberFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  function allDiceNumber() {
-    const allDiceArray = [];
-    for (let i = 0; i < 10; i++) {
-      allDiceArray.push(randomNumberFromInterval(1, 6));
-    }
-    return allDiceArray;
-  }
   return (
     <div className="App">
-      <div className="dice-grid">
-        {dices.map((diceNumber, index) => (
-          <Dice key={index} value={diceNumber} />
-        ))}
-      </div>
+      <DiceContainer />
     </div>
   );
 }
