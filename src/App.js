725d9569b0d36608776f7735e3d91fcf32a10d92
Raffay Syed <raffayhusayn@gmail.com>
2022-08-02 00:51:59 -0400

    Add logic to create a Array of Random Dice Numbers of 10 and using it to render 10 Dice component taking the values from the Array as props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import "./App.css";
 import Dice from "./components/Dice";
 
 function App() {
+  function randomNumberFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  function allDiceNumber() {
+    const allDiceArray = [];
+    for (let i = 0; i < 10; i++) {
+      allDiceArray.push(randomNumberFromInterval(1, 6));
+    }
+    return allDiceArray;
+  }
+  const allDices = allDiceNumber();
   return (
     <div className="App">
       <div className="dice-grid">
-        <Dice value="2" />
-        <Dice value="1" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
-        <Dice value="2" />
+        {allDices.map((diceNumber, index) => (
+          <Dice key={index} value={diceNumber} />
+        ))}
       </div>
     </div>
   );
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,7 +1,7 @@
 import "../styles/Dice.css";
 function Dice(props) {
   return (
-    <div className="dice">
+    <div className="dice-face">
       <h2>{props.value}</h2>
     </div>
   );
