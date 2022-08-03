e6a4b7a647a70ebbe12220a4cf48c845750d7360
Raffay Syed <raffayhusayn@gmail.com>
2022-08-03 02:14:03 -0400

    Add code to conditionally render different className for the Dice component and adding css styling to that className
    -- Conditionally rendering dice to be green if they have isHeld property
    set to `true`

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,7 +1,7 @@
 import "../styles/Dice.css";
 function Dice(props) {
   return (
-    <div className="dice-face">
+    <div className={"dice-face " + (props.isHeld ? "dice-held" : "")}>
       <h2>{props.value}</h2>
     </div>
   );
