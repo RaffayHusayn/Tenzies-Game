1e55756a2c9377e114b5f4996505b3b87809acfa
Raffay Syed <raffayhusayn@gmail.com>
2022-08-03 02:11:59 -0400

    Add code to change dices State which was just an array of numbers to an array of Objects with properties like:
    -- value -> the number that was previously there, randomly generated
    -- isHeld -> boolean used to see if a dice is held or not
    -- id -> using NanoId npm package

--- src/components/DiceContainer.jsx
+++ src/components/DiceContainer.jsx
@@ -18,16 +18,22 @@ function DiceContainer() {
   function allDiceNumber() {
     const allDiceArray = [];
     for (let i = 0; i < 10; i++) {
-      allDiceArray.push(randomNumberFromInterval(1, 6));
+      const diceObj = {
+        value: randomNumberFromInterval(1, 6),
+        isHeld: true,
+        id: nanoid(),
+      };
+      allDiceArray.push(diceObj);
     }
     return allDiceArray;
   }
 
+  console.log(dices);
   return (
     <div className="dice-container">
       <div className="dice-grid">
-        {dices.map((diceNumber) => (
-          <Dice key={nanoid()} value={diceNumber} />
+        {dices.map((dice) => (
+          <Dice key={dice.id} value={dice.value} isHeld={dice.isHeld} />
         ))}
       </div>
       <button className="roll-btn" onClick={rollDices}>
